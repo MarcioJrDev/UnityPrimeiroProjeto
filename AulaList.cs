0d0237d89c575d9fd5f35a03425c28067561da88
MarcioJrDev <120151023+MarcioJrDev@users.noreply.github.com>
2023-01-29 23:19:58 -0300

    Add files via upload

--- AulaList.cs
+++ AulaList.cs
@@ -2,14 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AulaFor : MonoBehaviour
+public class AulaList : MonoBehaviour
 {
+
+    public List<int> idade = new List<int>();
+
+
     void Start()
     {
-       
+       idade.Add(1);
+       idade.Add(2);
+       idade.Add(3);
+
+    
+    foreach (int item in idade)
+    {
+        Debug.Log(item);
     }
+    }
+
     void Update()
     {
-       
+
     }
 }
